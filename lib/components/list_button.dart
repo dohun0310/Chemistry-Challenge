a588a20372c57baa50ae8e0ab6c4cf6a99f97cbc
dohun0310 <dohun0310@naver.com>
2024-01-03 22:20:58 +0900

    edit list button

--- lib/components/list_button.dart
+++ lib/components/list_button.dart
@@ -3,15 +3,17 @@ import 'package:flutter/material.dart';
 class ListButton extends StatelessWidget {
   final String title;
   final String difficulty;
+  final MaterialColor difficultyColor;
   final int questionnum;
+  final MaterialColor questionnumColor;
   final Widget destinationPage;
   
-  const ListButton({super.key, required this.title, required this.difficulty, required this.questionnum, required this.destinationPage});
+  const ListButton({super.key, required this.title, required this.difficulty, required this.difficultyColor, required this.questionnum, required this.questionnumColor, required this.destinationPage});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(bottom: 12),
+      margin: const EdgeInsets.fromLTRB(8, 0, 8, 12),
       child: ElevatedButton(
         onPressed: () {
           Navigator.push(
@@ -24,57 +26,73 @@ class ListButton extends StatelessWidget {
           foregroundColor: Colors.black,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(16),
+            side: const BorderSide(color: Colors.grey, width: 1),
           ),
+          elevation: 0,
         ),
         child: Padding(
-          padding: const EdgeInsets.all(12.0), // Adjust the padding value as needed
+          padding: const EdgeInsets.all(8.0),
           child: Row(
             children: [
               Expanded(
                 child: Text(
                   title,
                   style: const TextStyle(
-                    fontSize: 24,
+                    fontSize: 20,
                   ),
                 ),
               ),
               Row(
                 children: [
-                  Column(
-                    children: [
-                      const Text(
-                        "난이도",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                    decoration: BoxDecoration(
+                      color: difficultyColor,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Column(
+                      children: [
+                        const Text(
+                          "난이도",
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      Text(
-                        difficulty,
-                        style: const TextStyle(
-                          fontSize: 16,
+                        Text(
+                          difficulty,
+                          style: const TextStyle(
+                            fontSize: 12,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                   const SizedBox(width: 12),
-                  Column(
-                    children: [
-                      const Text(
-                        "문제 수",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                    decoration: BoxDecoration(
+                      color: questionnumColor,
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Column(
+                      children: [
+                        const Text(
+                          "문제 수",
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      Text(
-                        questionnum.toString(),
-                        style: const TextStyle(
-                          fontSize: 16,
+                        Text(
+                          questionnum.toString(),
+                          style: const TextStyle(
+                            fontSize: 12,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
+                      ],
+                    ),
+                  )
                 ],
               ),
             ],
