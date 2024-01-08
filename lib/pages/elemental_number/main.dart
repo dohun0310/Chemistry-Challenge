45d54f3b98862deecb49babb5852c1de667ad2e8
dohun0310 <dohun0310@naver.com>
2024-01-08 22:14:09 +0900

    add challenge elemantal number

---
+++ lib/pages/elemental_number/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:elemental_challenge/components/challenge_page.dart';
+import 'package:elemental_challenge/data/elemental_number.dart';
+
+class ChallengeElementalNumber extends StatelessWidget {
+  const ChallengeElementalNumber({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '원소 번호 챌린지',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ChallengePage(
+        title: '원소 번호 챌린지',
+        description: '이 원소 번호에 맞는 원자는 무엇인가요?',
+        challengeData: elementalnumber,
+        questionItem: 'number',
+        answerItem: 'symbol',
+      ),
+    );
+  }
+}
