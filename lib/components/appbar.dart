83f0214fcf91846c52eaa232729f6ad1b55a87a0
dohun0310 <dohun0310@naver.com>
2024-01-27 23:23:59 +0900

    edit appbar text styling

--- lib/components/appbar.dart
+++ lib/components/appbar.dart
@@ -6,7 +6,7 @@ AppBar buildAppBar(BuildContext context, String title) {
   return AppBar(
     title: Text(
       title,
-      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+      style: Theme.of(context).textTheme.titleMedium!.copyWith(
         color: Theme.of(context).extension<ThemeColors>()?.onBackground,
       ),
     ),
